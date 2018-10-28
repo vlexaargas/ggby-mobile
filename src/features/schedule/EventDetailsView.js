5523fa3234d2b2b0929054b307947afed02809df
yepster <yepster1@live.co.za>
2018-10-28 16:17:00 -0700

    started getting data from sheet -> app

--- src/features/schedule/EventDetailsView.js
+++ src/features/schedule/EventDetailsView.js
@@ -5,26 +5,26 @@ import { ScrollView, View, Text, Image } from "react-native";
 
 import styles from "./EventDetailsView.style";
 
-const event = {
-  title: "Slackro",
-  details:
-    "You may have tried Acroyoga before, but perhaps you haven't tried the type of acroyoga that's designed specifically for YOUR strengths. Slackro is a type of acroyoga that utilizes the sort of balance and focus skill that you EXCEL at because of your slackline practice. Come balance and wiggle on other humans in between wiggling on one-inch bridges.",
-  picRef: "slackro",
-  instructors: [
-    {
-      name: "Michelle",
-      picRef: "michelle"
-    },
-    {
-      name: "Alex",
-      picRef: "alex"
-    },
-    {
-      name: "Brian",
-      picRef: "brian"
-    }
-  ]
-};
+// const event = {
+//   title: "Slackro",
+//   longDetails:
+//     "You may have tried Acroyoga before, but perhaps you haven't tried the type of acroyoga that's designed specifically for YOUR strengths. Slackro is a type of acroyoga that utilizes the sort of balance and focus skill that you EXCEL at because of your slackline practice. Come balance and wiggle on other humans in between wiggling on one-inch bridges.",
+//   picRef: "slackro",
+//   instructors: [
+//     {
+//       name: "Michelle",
+//       picRef: "michelle"
+//     },
+//     {
+//       name: "Alex",
+//       picRef: "alex"
+//     },
+//     {
+//       name: "Brian",
+//       picRef: "brian"
+//     }
+//   ]
+// };
 
 // TODO: Extract into own service
 const eventPicMap = {
@@ -38,36 +38,66 @@ const instructorPicMap = {
   brian: require("../../../assets/images/events/slackro.jpg")
 };
 
-export default ({ navigation }) => (
-  // TODO: Uncomment this.
-  // const event = navigation.getParam("event");
+function getPicForEvent(title) {
+  //This is just an example will be changed once we populate the maps
+  return eventPicMap['slackro']
+  // return eventPicMap[title];
+}
 
-  <ScrollView>
-    <Image style={styles.eventImage} source={eventPicMap[event.picRef]} />
+function getInstructorList(event) {
+  //This is just an example will be changed once we populate the maps
+  return ['alex']
+  //return event.instructors.replace(' ','').split(',').map(i -> {return {name: i}});
+}
+
+function getPicForInstructor(instructorName) {
+  //This is just an example will be changed once we populate the maps
+  return instructorPicMap['michelle']
+  // return instructorPicMap[instructorName];
+}
+
+function getDescription(event) {
+  if (event.longDescription == '' || event.longDescription == undefined) {
+    return event.description;
+  }
+  return event.longDescription;
+}
+
+function getRender(navigation) {
+  var event = navigation.getParam("event");
+
+  return <ScrollView>
+    <Image style={styles.eventImage} source={getPicForEvent(event.title)} />
 
     <View style={styles.contentContainer}>
       <Text style={styles.titleText}>{event.title}</Text>
-      <Text style={styles.normalText}>{event.details}</Text>
+      <Text style={styles.normalText}>{event.longDescription}</Text>
 
       <View style={styles.instructorsSection}>
         <Text style={styles.secondaryTitleText}>Taught by:</Text>
 
         <View style={styles.instructorListView}>
           {map(
-            ({ name, picRef }) => (
-              <View style={styles.instructorView} key={picRef}>
+            ({ name }) => (
+              console.log(name)
+              <View style={styles.instructorView} key={getPicForEvent(event.title)}>
                 <Image
                   style={styles.instructorImage}
-                  source={instructorPicMap[picRef]}
+                  source={getPicForInstructor(name)}
                 />
 
                 <Text style={styles.normalText}>{name}</Text>
               </View>
             ),
-            event.instructors
+            getInstructorList(event)
           )}
         </View>
       </View>
     </View>
   </ScrollView>
-);
+  
+}
+
+export default ({ navigation }) => (
+    getRender(navigation)
+  );
