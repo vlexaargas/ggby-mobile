c6abe8cbaf7509c9ab6f4976860d95315ce4bc96
Guillaume Labranche <gumgl@users.noreply.github.com>
2018-11-06 00:48:31 -0800

    Add Sentry crash reporting

--- App.js
+++ App.js
@@ -6,6 +6,8 @@ import { PersistGate } from "redux-persist/integration/react";
 import RootNavigator from "./src/features/rootNavigator";
 import configureStore from "./src/store/configureStore";
 
+import Sentry from 'sentry-expo';
+
 const { store, persistor } = configureStore();
 
 export default () => (
@@ -13,3 +15,8 @@ export default () => (
     <PersistGate persistor={persistor}>{() => <RootNavigator />}</PersistGate>
   </Provider>
 );
+
+// Remove this once Sentry is correctly setup.
+Sentry.enableInExpoDevelopment = true;
+
+Sentry.config('https://9b46bad3a3e44959845540e33f723584@sentry.io/1316473').install();
