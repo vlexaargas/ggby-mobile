fbb3f8971606cb0f4c8c731ebe14c4e871bfff2b
Guillaume Labranche <gumgl@users.noreply.github.com>
2018-11-07 01:49:59 -0800

    Update map to 2nd version

--- src/features/map/MapNavigator.js
+++ src/features/map/MapNavigator.js
@@ -12,7 +12,7 @@ const Map = () => (
       width: undefined,
       height: undefined
     }}
-    source={require("../../../assets/images/hand-map.jpg")}
+    source={require("../../../assets/images/hand-map-2.jpg")}
   />
 );
 
