9297f9e040565cf357e37df2dae3cd7db66ed512
Guillaume Labranche <gumgl@users.noreply.github.com>
2018-11-06 23:14:11 -0800

    Remove unused imports

--- src/features/map/MapNavigator.js
+++ src/features/map/MapNavigator.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { View, Text, Image, WebView } from "react-native";
+import { WebView } from "react-native";
 import { createStackNavigator } from "react-navigation";
 import defaultStackNavigatorConfigs from "../../components/navigator";
 
