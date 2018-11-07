10f8292826b6500ca6e8a080506034dbcbe047df
Guillaume Labranche <gumgl@users.noreply.github.com>
2018-11-06 23:12:35 -0800

    Make map scrollable

--- src/features/map/MapNavigator.js
+++ src/features/map/MapNavigator.js
@@ -1,18 +1,15 @@
 import React from "react";
 
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, WebView } from "react-native";
 import { createStackNavigator } from "react-navigation";
 import defaultStackNavigatorConfigs from "../../components/navigator";
 
 const Map = () => (
-  <Image
-    style={{
-      flex: 1,
-      resizeMode: 'cover',
-      width: undefined,
-      height: undefined
-    }}
-    source={require("../../../assets/images/hand-map.jpg")}
+  <WebView
+  originWhitelist={['*']}
+  source={require("../../../assets/images/hand-map-2.jpg")}
+  injectedJavaScript={`const meta = document.createElement('meta'); meta.setAttribute('content', 'width=device-width, initial-scale=0.16, minimum-scale=0.16, maximum-scale=0.4, user-scalable=1'); meta.setAttribute('name', 'viewport'); document.getElementsByTagName('head')[0].appendChild(meta); `}
+  scalesPageToFit={false}
   />
 );
 
