d23f5956e3afff7748ae10cc655d8cffd64e4ccd
Guillaume Labranche <gumgl@users.noreply.github.com>
2018-11-07 01:18:16 -0800

    Revert "Make map scrollable"
    This reverts commit 10f8292826b6500ca6e8a080506034dbcbe047df.

--- src/features/map/MapNavigator.js
+++ src/features/map/MapNavigator.js
@@ -5,11 +5,14 @@ import { createStackNavigator } from "react-navigation";
 import defaultStackNavigatorConfigs from "../../components/navigator";
 
 const Map = () => (
-  <WebView
-  originWhitelist={['*']}
-  source={require("../../../assets/images/hand-map-2.jpg")}
-  injectedJavaScript={`const meta = document.createElement('meta'); meta.setAttribute('content', 'width=device-width, initial-scale=0.16, minimum-scale=0.16, maximum-scale=0.4, user-scalable=1'); meta.setAttribute('name', 'viewport'); document.getElementsByTagName('head')[0].appendChild(meta); `}
-  scalesPageToFit={false}
+  <Image
+    style={{
+      flex: 1,
+      resizeMode: 'cover',
+      width: undefined,
+      height: undefined
+    }}
+    source={require("../../../assets/images/hand-map.jpg")}
   />
 );
 
