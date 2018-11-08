5ba6cbc28b0253bdba26940d330cbdf3a3216298
Alex Vargas <vlexaargas@gmail.com>
2018-11-08 00:12:32 -0500

    Alex pdf test.

--- src/features/info/InfoContainer.js
+++ src/features/info/InfoContainer.js
@@ -1,11 +1,18 @@
 import React from "react";
+import PDFReader from "rn-pdf-reader-js";
+import { Asset } from "expo";
+// import InfoView from "./InfoView";
 
-import InfoView from "./InfoView";
+const pdfAsset = Asset.fromModule(
+  require("../../../assets/ggby-volunteer-guide.pdf")
+);
 
+const pdfUri = pdfAsset.localUri || pdfAsset.uri;
 
 class InfoContainer extends React.Component {
   render() {
-    return <InfoView />;
+    // return <InfoView />;
+    return <PDFReader source={{ uri: pdfUri }} />;
   }
 }
 
