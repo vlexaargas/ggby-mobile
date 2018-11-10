b369347c3102fe3a17b13c004752e7c49ca7e2c4
Daniel Schlitt <dschlitt2@gmail.com>
2018-11-09 23:33:23 -0800

    1st Publication Updates (#27)
    Update the app.json manifest with publication requirements (version numbers, permissions, etc)
    Add the right size app icons
    remove Sentry for the time being because we aren't notifying users that we'll be collecting crash reports

--- App.js
+++ App.js
@@ -6,8 +6,6 @@ import { PersistGate } from "redux-persist/integration/react";
 import RootNavigator from "./src/features/rootNavigator";
 import configureStore from "./src/store/configureStore";
 
-import Sentry from 'sentry-expo';
-
 const { store, persistor } = configureStore();
 
 export default () => (
@@ -15,8 +13,3 @@ export default () => (
     <PersistGate persistor={persistor}>{() => <RootNavigator />}</PersistGate>
   </Provider>
 );
-
-// Uncomment this line to log in the local Expo app
-//Sentry.enableInExpoDevelopment = true;
-
-Sentry.config('https://9b46bad3a3e44959845540e33f723584@sentry.io/1316473').install();
