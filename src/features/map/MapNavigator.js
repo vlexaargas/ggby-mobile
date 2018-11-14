b58ed7e2f34b6a0cccff3e2ff3ce29a7775076c7
Danny Schlitt <dschlitt2@gmail.com>
2018-11-14 02:54:29 -0800

    basic zoom + pan

--- src/features/map/MapNavigator.js
+++ src/features/map/MapNavigator.js
@@ -7,23 +7,29 @@ import defaultStackNavigatorConfigs from "../../components/navigator";
 
 const Map = () => (
 
-   <ImageZoom cropWidth={Dimensions.get('screen').width}
-                       cropHeight={Dimensions.get('screen').height}
-                       imageWidth={1988}
-                       imageHeight={2547}
-                       enableCenterFocus={false}
-                       centerOn={{ x: 0, y: 0, scale: 0.25, duration: 100 }}
-                       minScale={0.2}
-                       maxScale={2.0}
-                       maxOverflow={0}>
-                <Image
-                style={{
-                  width: 1988,
-                  height: 2547
-                }}
-                source={require("../../../assets/images/the-beautiful-fucking-map.png")}
-                />
-            </ImageZoom>
+  <ImageZoom // this is very brittle with respect to making a general slackline festival app. This code is specific to the GGBY file map. TODO -- clean up
+    cropWidth={Dimensions.get('screen').width}
+    cropHeight={Dimensions.get('screen').height}
+    imageWidth={1988}
+    imageHeight={2547}
+    enableCenterFocus={false}
+    centerOn={{ x: 0, y: 0, scale: 0.17, duration: 100 }} //yuck TODO no magic numbers
+    minScale={0.1}
+    maxscale={2.0} 
+    maxoverflow={0}
+    style={{
+      flex: 1,
+      resizemode: 'contain',
+      alignSelf: 'center'
+      }}>
+     <Image 
+       style={{
+         width: 1988, // yuck TODO no magics numbers
+         height: 2547
+       }}
+       source={require("../../../assets/images/the-beautiful-fucking-map.png")}
+     />
+  </ImageZoom>
   
 );
 
