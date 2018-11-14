92ea4afe53098b45512ea7ffa1bb6b94f67de951
Daniel Schlitt <dschlitt2@gmail.com>
2018-11-14 03:00:37 -0800

    Basic Zooming Working -- Not Perfect (#29)
    Use react-native-image-pan-zoom to achieve simple pan and zoom.

--- src/features/map/MapNavigator.js
+++ src/features/map/MapNavigator.js
@@ -1,19 +1,36 @@
 import React from "react";
 
-import { View, Text, Image, WebView } from "react-native";
+import { Image, Dimensions } from "react-native";
+import ImageZoom from 'react-native-image-pan-zoom';
 import { createStackNavigator } from "react-navigation";
 import defaultStackNavigatorConfigs from "../../components/navigator";
 
 const Map = () => (
-  <Image
+
+  <ImageZoom // this is very brittle with respect to making a general slackline festival app. This code is specific to the GGBY file map. TODO -- clean up
+    cropWidth={Dimensions.get('screen').width}
+    cropHeight={Dimensions.get('screen').height}
+    imageWidth={1988}
+    imageHeight={2547}
+    enableCenterFocus={false}
+    centerOn={{ x: 0, y: 0, scale: 0.17, duration: 100 }} //yuck TODO no magic numbers
+    minScale={0.1}
+    maxscale={2.0} 
+    maxoverflow={0}
     style={{
       flex: 1,
-      resizeMode: 'contain',
-      width: undefined,
-      height: undefined
-    }}
-    source={require("../../../assets/images/the-beautiful-fucking-map.png")}
-  />
+      resizemode: 'contain',
+      alignSelf: 'center'
+      }}>
+     <Image 
+       style={{
+         width: 1988, // yuck TODO no magics numbers
+         height: 2547
+       }}
+       source={require("../../../assets/images/the-beautiful-fucking-map.png")}
+     />
+  </ImageZoom>
+  
 );
 
 const MapNavigator = createStackNavigator(
