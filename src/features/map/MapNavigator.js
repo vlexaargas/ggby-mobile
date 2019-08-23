4b05eabfc51920775f2e673c7d7337fa4a797a9d
Alex Vargas <vlexaargas@gmail.com>
2019-08-22 19:00:34 -0600

    Add fallback to simple image if image zoom crashes.

--- src/features/map/MapNavigator.js
+++ src/features/map/MapNavigator.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-import { Image, Dimensions } from "react-native";
+import { Image, Dimensions, StyleSheet } from "react-native";
 import ImageZoom from "react-native-image-pan-zoom";
+
 import {
   createStackNavigator,
   createMaterialTopTabNavigator
 } from "react-navigation";
-import { StyleSheet } from "react-native";
+
 import defaultStackNavigatorConfigs from "../../components/navigator";
 
 import * as v from "../../theme/variables";
@@ -26,27 +27,22 @@ const styles = StyleSheet.create({
 const mapWidth = 1080;
 const mapHeight = 1750;
 // There are a lot of magic numbers in this file.
-// The mapWidth and height must be included as well as the magic numbers 
+// The mapWidth and height must be included as well as the magic numbers
 // for the crop width and height :/
 
 // Also convert to stateful component so reset() can be called when component will mount.
-// without this, cropWidth 
-const CityMap = ({viewPortWidth, ViewPortHeight}) => (
-  <ImageZoom
-    cropWidth={Dimensions.get("screen").width}
-    cropHeight={Dimensions.get("screen").height-240}
-    imageWidth={mapWidth}
-    imageHeight={mapHeight}
-    enableCenterFocus={false}
-    centerOn={{ x: 0, y: 0, scale: .3, duration: 100 }} 
-    minScale={0.2}
-    maxscale={2.0}
-    maxoverflow={0}
-    style={{
-      flex: 1,
-      alignSelf: "stretch"
-    }}
-  >
+// without this, cropWidth
+class CityMap extends React.Component {
+  state = {
+    error: false
+  };
+
+  static getDerivedStateFromError() {
+    // Update state so the next render will show the fallback UI.
+    return { error: true };
+  }
+
+  renderImage = () => (
     <Image
       style={{
         width: mapWidth,
@@ -54,28 +50,49 @@ const CityMap = ({viewPortWidth, ViewPortHeight}) => (
       }}
       source={require("../../../assets/images/festival-map.png")}
     />
-  </ImageZoom>
-);
+  );
+
+  render() {
+    const { error } = this.state;
+
+    return !error ? (
+      <ImageZoom
+        cropWidth={Dimensions.get("screen").width}
+        cropHeight={Dimensions.get("screen").height - 240}
+        imageWidth={mapWidth}
+        imageHeight={mapHeight}
+        enableCenterFocus={false}
+        centerOn={{ x: 0, y: 0, scale: 0.3, duration: 100 }}
+        minScale={0.2}
+        maxscale={2.0}
+        maxoverflow={0}
+        style={{
+          flex: 1,
+          alignSelf: "stretch"
+        }}
+      >
+        {this.renderImage()}
+      </ImageZoom>
+    ) : (
+      this.renderImage()
+    );
+  }
+}
 
 const map2Width = 613;
 const map2Height = 280;
 
-const FestivalMap = () => (
-  <ImageZoom // this is very brittle with respect to making a general slackline festival app. This code is specific to the GGBY file map. TODO -- clean up
-    cropWidth={Dimensions.get("screen").width}
-    cropHeight={Dimensions.get("screen").height - 240}
-    imageWidth={map2Width}
-    imageHeight={map2Height}
-    enableCenterFocus={false}
-    centerOn={{ x: 0, y: 0, scale: 0.5, duration: 100 }} // yuck TODO no magic numbers
-    minScale={0.5}
-    maxscale={1.0}
-    maxoverflow={0}
-    style={{
-      flex: 1,
-      alignSelf: "stretch"
-    }}
-  >
+class FestivalMap extends React.Component {
+  state = {
+    error: false
+  };
+
+  static getDerivedStateFromError() {
+    // Update state so the next render will show the fallback UI.
+    return { error: true };
+  }
+
+  renderImage = () => (
     <Image
       style={{
         width: map2Width, // yuck TODO no magics numbers
@@ -83,8 +100,34 @@ const FestivalMap = () => (
       }}
       source={require("../../../assets/images/details-map.png")}
     />
-  </ImageZoom>
-);
+  );
+
+  render() {
+    const { error } = this.state;
+
+    return !error ? (
+      <ImageZoom // this is very brittle with respect to making a general slackline festival app. This code is specific to the GGBY file map. TODO -- clean up
+        cropWidth={Dimensions.get("screen").width}
+        cropHeight={Dimensions.get("screen").height - 240}
+        imageWidth={map2Width}
+        imageHeight={map2Height}
+        enableCenterFocus={false}
+        centerOn={{ x: 0, y: 0, scale: 0.5, duration: 100 }} // yuck TODO no magic numbers
+        minScale={0.5}
+        maxscale={1.0}
+        maxoverflow={0}
+        style={{
+          flex: 1,
+          alignSelf: "stretch"
+        }}
+      >
+        {this.renderImage()}
+      </ImageZoom>
+    ) : (
+      this.renderImage()
+    );
+  }
+}
 
 const MapTabs = createMaterialTopTabNavigator(
   {
