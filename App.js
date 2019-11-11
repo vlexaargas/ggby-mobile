fd03e73e6b34cef629cd4625f401a26f0e344c2f
Alex Vargas <vlexaargas@gmail.com>
2019-11-10 21:08:22 -0700

    Install Sentry.

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import * as Sentry from "sentry-expo";
 
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
@@ -7,6 +8,12 @@ import RootNavigator from "./src/features/rootNavigator";
 import configureStore from "./src/store/configureStore";
 
 const { store, persistor } = configureStore();
+
+Sentry.init({
+  dsn: "https://9b46bad3a3e44959845540e33f723584@sentry.io/1316473",
+  enableInExpoDevelopment: true,
+  debug: true
+});
 
 export default () => (
   <Provider store={store}>
